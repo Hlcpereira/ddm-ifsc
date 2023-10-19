62b7bfb3507ed726d505725828f58df6a3dd89cb
Henrique Pereira <hlcpereira@outlook.com.br>
2023-10-19 20:44:48 -0300

    TTS + sqlite cause yes
    How make an app to annoy people  >:)

--- app/src/main/java/com/example/tts/MainActivity.java
+++ app/src/main/java/com/example/tts/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.tts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     public Button button;
     private boolean loadTTS;
 
+    private SQLiteDatabase banco;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +32,21 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
         tts = new TextToSpeech(this, this);
         loadTTS = false;
+
+        handleDatabase("Olá");
+        handleDatabase("Bom dia");
+        handleDatabase("Boa Tarde");
+        handleDatabase("Boa Noite");
+        handleDatabase("Até logo");
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (loadTTS){
-                    tts.speak(editText.getText().toString(), TextToSpeech.QUEUE_FLUSH, null, null);
+                Cursor cursor = banco.rawQuery("SELECT _id, text FROM text ", null);
+                cursor.moveToFirst();
+                while (cursor.moveToNext()){
+                    String text = cursor.getString(cursor.getColumnIndex("text"));
+                    speak(text);
                 }
             }
         });
@@ -43,5 +59,21 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         } else {
             Log.e("TTS", "TTS não inicializado");
         }
+    }
+
+    public void speak(String text){
+        if (loadTTS){
+            tts.speak(text, TextToSpeech.QUEUE_ADD, null, null);
+        }
+    }
+
+    public void handleDatabase(String text){
+        this.banco = openOrCreateDatabase("banco", getBaseContext().MODE_PRIVATE,null);
+
+        banco.execSQL("CREATE TABLE IF NOT EXISTS text (_id INTEGER PRIMARY KEY AUTOINCREMENT,text TEXT )" );
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("text",text);
+        banco.insert("text",null,contentValues );
     }
 }
